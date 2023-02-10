56d52383915896fc982023d89767df3c5b97520b
sammwaura <meshsami27@gmail.com>
2023-02-10 11:03:50 +0300

    modify files

--- js/main.js
+++ js/main.js
@@ -9,3 +9,24 @@ addBtn.onclick = function() {
 closeBtn.addEventListener("click", ()=>{
     modal.classList.remove("active");
 }); 
+
+var registerBtn = document.querySelector("#register-btn");
+var userData = [];
+var prodId = document.getElementById("id");
+var prodName = document.getElementById("#name");
+var prodQuantity = document.getElementById("#quantity");
+
+registerBtn.onclick = function(){
+    alert("registered!");
+    registerData();
+}
+
+function registerData(){
+    userData.push({
+        id : prodId.value,
+        name : prodName.value,
+        quantity : prodQuantity,
+    });
+    var userString = JSON.stringify(userData);
+    localStorage.setItem("userData", userString);
+}
