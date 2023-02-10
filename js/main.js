816cdf98505a69cd3041610865d3f8a19e5ef966
sammwaura <meshsami27@gmail.com>
2023-02-10 16:33:57 +0300

    modify files

--- js/main.js
+++ js/main.js
@@ -15,17 +15,46 @@ var userData = [];
 var idProd = document.getElementById("id");
 var nameProd = document.querySelector("#name");
 var quantityProd = document.querySelector("#quantity");
+var registerForm = document.querySelector("#register-form");
+// var imgUrl;
 
-registerBtn.addEventListener("click", ()=>{
+registerBtn.onclick = function(e){
+    e.preventDefault();
     registerData();
-})
+    registerForm.reset('');
+    closeBtn.click();
+}
+
+if (localStorage.getItem("userData") != null){
+    userData = JSON.parse(localStorage.getItem("userData"));
+}
 
 function registerData(){
     userData.push({
         id : idProd.value,
         name : nameProd.value,
         quantity : quantityProd.value
+        // IMg : imgUrl == undefined ? "images/loafers.jpg" : imgUrl
     });
     var userString = JSON.stringify(userData);
     localStorage.setItem("userData", userString);
 }
+var tableData = document.querySelector("#table-data");
+const getDataFromLocal = () =>{
+    userData.forEach((data, index)=>{
+        tableData.innerHTML = `
+                        <tr>
+                                    <td>Prod num</td>
+                                    <td>Prod num</td>
+                                    <td>Prod num</td>
+                                    <td>Prod num</td>
+                                    <td>
+                                        <button><i class="fa fa-eye"></i></button>
+                                        <button  style="background-color: burlywood;"><i class="fa fa-trash"></i></button>
+                                    </td>
+                                </tr>
+                                 `;
+    });
+}
+
+getDataFromLocal();
