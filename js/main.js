ae8ed6b38da6954ce0615e3ddf7a4dfc996310d8
sammwaura <meshsami27@gmail.com>
2023-02-13 10:48:10 +0300

    modify files

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ var idProd = document.getElementById("id");
 var nameProd = document.querySelector("#name");
 var quantityProd = document.querySelector("#quantity");
 var registerForm = document.querySelector("#register-form");
-// var imgUrl;
+var prodPic;
 
 registerBtn.onclick = function(e){
     e.preventDefault();
@@ -33,28 +33,34 @@ function registerData(){
     userData.push({
         id : idProd.value,
         name : nameProd.value,
-        quantity : quantityProd.value
-        // IMg : imgUrl == undefined ? "images/loafers.jpg" : imgUrl
+        quantity : quantityProd.value,
+        prodPic : "images/loafers.jpg"
     });
     var userString = JSON.stringify(userData);
     localStorage.setItem("userData", userString);
 }
+// retrive data from local storage 
+
 var tableData = document.querySelector("#table-data");
 const getDataFromLocal = () =>{
-    userData.forEach((data, index)=>{
-        tableData.innerHTML = `
-                        <tr>
-                                    <td>Prod num</td>
-                                    <td>Prod num</td>
-                                    <td>Prod num</td>
-                                    <td>Prod num</td>
-                                    <td>
-                                        <button><i class="fa fa-eye"></i></button>
-                                        <button  style="background-color: burlywood;"><i class="fa fa-trash"></i></button>
-                                    </td>
-                                </tr>
-                                 `;
+    // tableData.innerHTML = "";
+    userData.forEach((data,index)=>{
+        tableData.innerHTML += `
+        <tr>
+            <td>${index+1}</td>
+            <td>${data.id}</td>
+            <td><img src = ${data.prodPic} width="40" height="40"></td>
+            <td>${data.name}</td>
+            <td>${data.quantity}</td>
+            <td>
+                <button><i class="fa fa-eye"></i></button>
+                <button  style="background-color: burlywood;"><i class="fa fa-trash"></i></button>
+            </td>
+        </tr>
+        
+        `;
     });
 }
-
 getDataFromLocal();
+
+                                
